3c4426bb69a3986392b8225a8b9dd25d8b94a403
koreankahu <ceoclub00@naver.com>
2021-02-05 21:47:05 +0900

    Change divide function as premium

--- calculator.py
+++ calculator.py
@@ -6,11 +6,7 @@ def subtract(a, b):
 	return a - b
 
 def multiply(a, b):
-<<<<<<< HEAD
 	return a * b
 
-def divid(a, b):
+def divid_premium(a, b):
 	return a / b
-=======
-	return a * b
->>>>>>> 3715eb29eaaccd2e8d44619ae89511d1f1dc18e6
